be9b2994a26ebd9d7272d713f8a06be47015acf8
sergey80401 <sergey80401@gmail.com>
2022-07-19 10:06:47 +0600

    Some changes

--- init.sh
+++ init.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-mkdir public uploads etc
-cd public
-mkdir img css js
-
-sudo su
-
-mv nginx.conf /home/box/web/etc/nginx.conf
-
 ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 /etc/init.d/nginx restart
